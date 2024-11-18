fbd0157300b1a8d62e3edc3b39f24802b51239b1
Osmanovvv <server.osmanov26@mail.ru>
2024-11-19 01:04:08 +0300

    сделана большая часть секции new

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,33 @@ const clearInput = () => {
   input.value = "";
 }
 
+// Slider
+// Slider
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+  responsive: {
+    // breakpoint from 0 up
+    loop: true,
+    0: {
+      items: 2,
+      margin: 32,
+      loop: true,
+      dots: false,
+    },
+    // breakpoint from 480 up
+    601: {
+      items: 3,
+      margin: 32,
+      loop: true,
+      dots: false,
+    },
+    // breakpoint from 768 up
+    1001: {
+      items: 4,
+      // margin: 1,
+      dots: false,
+      loop: true,
+    },
+  },
+});
+});
